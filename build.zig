fa0228d8b255fe1f9399d78a7a8132ec716db116
Niklas Koll <nikkol567@gmail.com>
2024-08-06 16:04:10 +0200

    Added sqlite3 files

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const httpz = b.dependency("httpz", .{ .target = target, .optimize = .ReleaseSafe });
 
     const exe = b.addExecutable(.{
         .name = "zurl",
@@ -10,43 +11,33 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    const httpz_module = b.dependency("httpz", .{ .target = target, .optimize = optimize }).module("httpz");
-    exe.root_module.addImport("httpz", httpz_module);
-
-    // exe.linkLibC();
-    exe.linkSystemLibrary("sqlite3");
+    exe.root_module.addImport("httpz", httpz.module("httpz"));
+    exe.linkLibC();
+    exe.addCSourceFile(.{ .file = b.path("lib/sqlite3.c") });
+    exe.addIncludePath(b.path("lib"));
 
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
-
     run_cmd.step.dependOn(b.getInstallStep());
-
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
-
+    // Check step for better lsp diagnostics
     const exe_check = b.addExecutable(.{
         .name = "zurl",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = .Debug,
     });
-    exe_check.root_module.addImport("httpz", httpz_module);
+    exe_check.root_module.addImport("httpz", httpz.module("httpz"));
+    exe_check.linkLibC();
+    exe_check.addCSourceFile(.{ .file = b.path("lib/sqlite3.c") });
+    exe_check.addIncludePath(b.path("lib"));
+
     const check = b.step("check", "Check if compiles");
     check.dependOn(&exe_check.step);
 }
